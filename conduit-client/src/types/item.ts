751e1cb26177cac069b59a5455beae61745b9011
nguyenvannhatduy <160450843+nguyenvannhatduy@users.noreply.github.com>
2024-07-26 22:12:13 +0700

    initial source
    start project

--- conduit-client/src/types/item.ts
+++
@@ -1,3 +0,0 @@
-export type GetItemParams = {
-  packed?: boolean;
-};
--- conduit-client/src/utils/item.ts
+++
@@ -1,5 +0,0 @@
-export const getFilterParams = (selectedFilter: { value: string }) => {
-  if (selectedFilter.value === "packed") return { packed: true };
-  if (selectedFilter.value === "unpacked") return { packed: false };
-  return { packed: undefined };
-};
